1cd7ec5983cb046e6d47dc0712d666e16dcfcf87
Michael Weems <mweems5@gmail.com>
2024-03-31 16:02:49 -0500

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: 
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      nixosConfigurations = {
+        default = nixpkgs.lib.nixosSystem {
+          specialArgs = {inherit inputs;};
+          modules = [
+            ./hosts/default/configuration.nix
+          ];
+        };
+        coding = nixpkgs.lib.nixosSystem {
+          specialArgs = {inherit inputs;};
+          modules = [
+            ./hosts/coding/configuration.nix
+          ];
+        };
+        gaming = nixpkgs.lib.nixosSystem {
+          specialArgs = {inherit inputs;};
+          modules = [
+            ./hosts/gaming/configuration.nix
+          ];
+        };
+        core-keeper = nixpkgs.lib.nixosSystem {
+          specialArgs = {inherit inputs;};
+          modules = [
+            ./hosts/core-keeper/configuration.nix
+          ];
+        };
+      };
+    };
+}
